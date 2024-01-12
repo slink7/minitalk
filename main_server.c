61432e23668a9dd6d6ce54db479705c91d4f8029
Sebastien Cambier <scambier@bess-f3r5s10.clusters.42paris.fr>
2024-01-12 16:55:43 +0100

    Added test main

--- main_server.c
+++ main_server.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 16:31:27 by scambier          #+#    #+#             */
-/*   Updated: 2024/01/12 14:35:46 by scambier         ###   ########.fr       */
+/*   Updated: 2024/01/12 16:01:35 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,18 +29,19 @@ void	f(int signum)
 	static int				count = 0;
 	int						bit;
 
-
 	if (signum != 10 && signum != 12)
 		return ;
 	bit = (signum - 10) / 2;
+	printf("now : %d, %d : ", byte, bit);
 	byte = right_bitshift_wrap(byte + bit);
+	printf("%d %d\n", bit, byte);
 	count++;
 	if (count < 8)
 		return ;
-	if (byte == 0)
-		write(1, "\n", 1);
-	else
-		write(1, &byte, 1);
+	// if (byte == 0)
+	// 	write(1, "\n", 1);
+	// else
+	write(1, &byte, 1);
 	byte = 0;
 	count = 0;
 }
@@ -53,7 +54,6 @@ int	main(int argc, char **argv)
 	signal(SIGUSR2, f);
 	pid = getpid();
 	printf("pid : %d\n", pid);
-
 	while (1)
 		usleep(0);
 }
