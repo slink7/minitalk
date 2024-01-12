30027235920722229f18c817e496b42f4d8eacbe
Sebastien Cambier <scambier@bess-f3r5s10.clusters.42paris.fr>
2024-01-12 14:48:58 +0100

    Added sending and receiving strings

--- main_client.c
+++ main_client.c
@@ -6,25 +6,53 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 16:32:04 by scambier          #+#    #+#             */
-/*   Updated: 2024/01/11 18:57:10 by scambier         ###   ########.fr       */
+/*   Updated: 2024/01/12 14:36:00 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
+
+#include <stdio.h>
 
 #include "libft.h"
+
+int	send_signum(int pid, int signum)
+{
+	int	out;
+
+	out = kill(pid, signum);
+	usleep(1000);
+	return (out);
+}
+
+void	send_char(int pid, char c)
+{
+	int	k;
+
+	k = -1;
+	printf("%c", c);
+	while (++k < 8)
+	{
+		send_signum(pid, 2 * (c % 2) + 10);
+		c = c >> 1;
+	}
+}
+
+void	send_str(int pid, char *str)
+{
+	while (*str)
+		send_char(pid, *str++);
+	send_char(pid, 0);
+}
 
 int	main(int argc, char **argv)
 {
 	int	pid;
 
-	if (argc != 2)
+	if (argc != 3)
 		return (0);
 	pid = ft_atoi(argv[1]);
-	kill(pid, SIGUSR1);
-	kill(pid, SIGUSR2);
-	kill(pid, SIGUSR1);
-	kill(pid, SIGUSR2);
-	kill(pid, SIGUSR2);
+	send_str(pid, argv[2]);
 }
--- main_server.c
+++ main_server.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 16:31:27 by scambier          #+#    #+#             */
-/*   Updated: 2024/01/11 20:19:57 by scambier         ###   ########.fr       */
+/*   Updated: 2024/01/12 14:35:46 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,31 @@
 
 #include "libft.h"
 
-// typedef void (*sighandler_t)(int);
+int	right_bitshift_wrap(unsigned char v)
+{
+	return ((v >> 1) | (v << 7));
+}
 
 void	f(int signum)
 {
-	printf("received %d\n", signum);
+	static unsigned char	byte = 0;
+	static int				count = 0;
+	int						bit;
+
+
+	if (signum != 10 && signum != 12)
+		return ;
+	bit = (signum - 10) / 2;
+	byte = right_bitshift_wrap(byte + bit);
+	count++;
+	if (count < 8)
+		return ;
+	if (byte == 0)
+		write(1, "\n", 1);
+	else
+		write(1, &byte, 1);
+	byte = 0;
+	count = 0;
 }
 
 int	main(int argc, char **argv)
@@ -33,5 +53,7 @@ int	main(int argc, char **argv)
 	signal(SIGUSR2, f);
 	pid = getpid();
 	printf("pid : %d\n", pid);
-	pause();
+
+	while (1)
+		usleep(0);
 }
