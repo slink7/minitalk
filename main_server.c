d83b2d22c09664a926506de8afbef357e8680212
slink <slink794@gmail.com>
2024-01-16 04:07:35 +0100

    Added forgotten things

--- main_server.c
+++ main_server.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 16:31:27 by scambier          #+#    #+#             */
-/*   Updated: 2024/01/16 03:51:18 by scambier         ###   ########.fr       */
+/*   Updated: 2024/01/16 04:03:20 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ typedef struct s_rstack
 	int	write_index;
 }	t_rstack;
 
+t_rstack	g_rs;
+
 void	rstack_write(t_rstack *rs, int k)
 {
 	rs->content[rs->write_index++ % MAX_SIG_BUFFER] = k;
@@ -42,8 +44,6 @@ int	right_bitshift_wrap(unsigned char v)
 {
 	return ((v >> 1) | (v << 7));
 }
-
-t_rstack	g_rs;
 
 void	interprete(int signum)
 {
@@ -66,15 +66,15 @@ void	interprete(int signum)
 	count = 0;
 }
 
-void	init_rs(t_rstack rs)
+void	init_rs(t_rstack *rs)
 {
 	int	k;
 
 	k = 0;
 	while (k > MAX_SIG_BUFFER)
-		rs.content[k] = 0;
-	rs.read_index = 0;
-	rs.write_index = 0;
+		rs->content[k] = 0;
+	rs->read_index = 0;
+	rs->write_index = 0;
 }
 
 void	sig_handler(int signum)
@@ -89,7 +89,7 @@ int	main(int argc, char **argv)
 	struct sigaction	sa;
 	int					pid;
 
-	init_rs(rs);
+	init_rs(&g_rs);
 	sa.sa_handler = &sig_handler;
 	sa.sa_flags = SA_RESTART;
 	sigaction(SIGUSR1, &sa, NULL);
@@ -97,32 +97,6 @@ int	main(int argc, char **argv)
 	pid = getpid();
 	printf("pid : %d\n", pid);
 	while (1)
-		if (rs.read_index < rs.write_index)
-			interprete(rstack_read(&rs));
+		if (g_rs.read_index < g_rs.write_index)
+			interprete(rstack_read(&g_rs));
 }
-/*
-void	handle(int sig)
-{
-	write(1, "Handled", 7);
-	if (sig == SIGUSR1)
-		write(1, " USR1\n", 6);
-	else if (sig == SIGUSR2)
-		write(1, " USR2\n", 6);
-}
-
-typedef struct sigaction t_sigaction;
-
-int	main(int argc, char **argv)
-{
-	printf("%d\n", getpid());
-	t_sigaction sa;
-	sa.sa_handler = &handle;
-	// sa.sa_flags = SA_RESTART;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-
-	while (1)
-		usleep(10000);
-	return (0);
-}
-*/
